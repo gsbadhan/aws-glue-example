933859f2715115f12e4c407cae901b7a5575b512
Gurpreet Singh <gurpreet.badhan.28@gmail.com>
2025-06-07 17:31:23 +0530

    Synchronizing job customers_glue_job from AWS Glue at 2025/06/07 12:01:23

--- source-to-target-data-load/customers_glue_job.py
+++ source-to-target-data-load/customers_glue_job.py
@@ -20,13 +20,13 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node Amazon S3, pull data from source table
+# pull data from source table
 AmazonS3_node1749294620672 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://test-gluespark/source-db/customers/"], "recurse": True}, transformation_ctx="AmazonS3_node1749294620672")
 
-# Script generated for node Amazon S3, load data to target table
+# Script generated for node Amazon S3
 EvaluateDataQuality().process_rows(frame=AmazonS3_node1749294620672, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1749294603932", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
 
-
+#  load data to target table
 AmazonS3_node1749294795612 = glueContext.write_dynamic_frame.from_options(frame=AmazonS3_node1749294620672, connection_type="s3", format="glueparquet", connection_options={"path": "s3://test-gluespark/target-db/customers/", "partitionKeys": []}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1749294795612")
 
 job.commit()
