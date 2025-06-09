2e71609cf7f28cbd64c8c72fdbb42b15990af1b4
Gurpreet Singh <gurpreet.badhan.28@gmail.com>
2025-06-09 11:46:15 +0530

    Synchronizing job customers_data_transffer from AWS Glue at 2025/06/09 06:16:14

--- spark-glue-without-crawler/customers_data_transffer.py
+++ spark-glue-without-crawler/customers_data_transffer.py
@@ -13,12 +13,7 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Default ruleset used by all target nodes with data quality enabled
-DEFAULT_DATA_QUALITY_RULESET = """
-    Rules = [
-        ColumnCount > 0
-    ]
-"""
+
 
 # read data from csv file
 sourcedb_customers = glueContext.create_dynamic_frame.from_options(
@@ -27,19 +22,12 @@ sourcedb_customers = glueContext.create_dynamic_frame.from_options(
     "recurse": True}
     )
 
-# # Script generated for node Amazon S3
-# EvaluateDataQuality().process_rows(
-#     frame=sourcedb_customers, 
-#     ruleset=DEFAULT_DATA_QUALITY_RULESET, 
-#     publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1749436542347", "enableDataQualityResultsPublishing": True}, 
-#     additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
-
 
 # write data to S3
 targetdb_customers = glueContext.write_dynamic_frame.from_options(
     frame=sourcedb_customers, connection_type="s3", format="glueparquet", 
     connection_options={"path": "s3://test-gluespark/target-db/customers/", "partitionKeys": []}, 
-    format_options={"compression": "uncompressed"}
+    format_options={"compression": "snappy"}
     )
 
 job.commit()
