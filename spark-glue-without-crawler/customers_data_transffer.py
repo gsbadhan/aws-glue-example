f35329a0db42a0694a70118f2a6767945d7bfcd2
Gurpreet Singh <gurpreet.badhan.28@gmail.com>
2025-06-09 11:39:15 +0530

    Synchronizing job customers_data_transffer from AWS Glue at 2025/06/09 06:09:15

--- spark-glue-without-crawler/customers_data_transffer.py
+++ spark-glue-without-crawler/customers_data_transffer.py
@@ -20,12 +20,26 @@ DEFAULT_DATA_QUALITY_RULESET = """
     ]
 """
 
-# Script generated for node Amazon S3
-AmazonS3_node1749436553262 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, connection_type="s3", format="csv", connection_options={"paths": ["s3://test-gluespark/source-db/customers/"], "recurse": True}, transformation_ctx="AmazonS3_node1749436553262")
+# read data from csv file
+sourcedb_customers = glueContext.create_dynamic_frame.from_options(
+    format_options={"quoteChar": "\"", "withHeader": True, "separator": ",", "optimizePerformance": False}, 
+    connection_type="s3", format="csv", connection_options={"paths": ["s3://test-gluespark/source-db/customers/"], 
+    "recurse": True}
+    )
 
-# Script generated for node Amazon S3
-EvaluateDataQuality().process_rows(frame=AmazonS3_node1749436553262, ruleset=DEFAULT_DATA_QUALITY_RULESET, publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1749436542347", "enableDataQualityResultsPublishing": True}, additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
+# # Script generated for node Amazon S3
+# EvaluateDataQuality().process_rows(
+#     frame=sourcedb_customers, 
+#     ruleset=DEFAULT_DATA_QUALITY_RULESET, 
+#     publishing_options={"dataQualityEvaluationContext": "EvaluateDataQuality_node1749436542347", "enableDataQualityResultsPublishing": True}, 
+#     additional_options={"dataQualityResultsPublishing.strategy": "BEST_EFFORT", "observations.scope": "ALL"})
 
-AmazonS3_node1749436648310 = glueContext.write_dynamic_frame.from_options(frame=AmazonS3_node1749436553262, connection_type="s3", format="glueparquet", connection_options={"path": "s3://test-gluespark/target-db/customers/", "partitionKeys": []}, format_options={"compression": "uncompressed"}, transformation_ctx="AmazonS3_node1749436648310")
+
+# write data to S3
+targetdb_customers = glueContext.write_dynamic_frame.from_options(
+    frame=sourcedb_customers, connection_type="s3", format="glueparquet", 
+    connection_options={"path": "s3://test-gluespark/target-db/customers/", "partitionKeys": []}, 
+    format_options={"compression": "uncompressed"}
+    )
 
 job.commit()
